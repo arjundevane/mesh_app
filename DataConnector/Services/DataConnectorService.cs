7b014f5e318b4ccf5f76a7e1be12ab134b724e52
Arjun Devane <arjundevane@gmail.com>
2023-03-11 16:09:49 -0500

    More Car RPCs

--- DataConnector/Services/DataConnectorService.cs
+++ DataConnector/Services/DataConnectorService.cs
@@ -1,4 +1,5 @@
 ﻿using CouchDB.Driver.Extensions;
+using DataConnector.DbContext;
 using Grpc.Core;
 using MeshApp.DataConnector.DbContext;
 using MeshApp.Error;
@@ -17,15 +18,15 @@ namespace DataConnector.Services
             _context = dbContext;
         }
 
-        public async override Task<CarResponse> GetCar(FindCarByIdRequest request, ServerCallContext context)
+        public async override Task<CarsResponse> FindCarsById(FindCarByIdRequest request, ServerCallContext context)
         {
             try
             {
                 var cars = await _context.Cars
-                .Where(c => c.Car.CarId == request.CarId)
-                .ToListAsync();
+                            .Where(c => c.Car.CarId == request.CarId.ToLower())
+                            .ToListAsync();
 
-                var response = new CarResponse();
+                var response = new CarsResponse();
                 response.Cars.AddRange(cars.Select(c => c.Car));
 
                 return response;
@@ -33,15 +34,73 @@ namespace DataConnector.Services
             catch (Exception ex)
             {
                 _logger.LogError(ex, $"Detected error while processing: {request}");
-                return new CarResponse
+                return new CarsResponse { Error = GenerateErrorResponse(ex) };
+            }
+        }
+
+        public async override Task<CarsResponse> FindCarsByMake(FindCarsByMakeRequest request, ServerCallContext context)
+        {
+            try
+            {
+                var cars = await _context.Cars
+                            .Where(c => c.Car.Make == request.Make.ToLower())
+                            .ToListAsync();
+
+                var response = new CarsResponse();
+                response.Cars.AddRange(cars.Select(c => c.Car));
+
+                return response;
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, $"Detected error while processing: {request}");
+                return new CarsResponse { Error = GenerateErrorResponse(ex) };
+            }
+        }
+
+        public async override Task<CarResponse> AddCar(AddCarRequest request, ServerCallContext context)
+        {
+            try
+            {
+                var newCar = new CarDocument
                 {
-                    Error = new Error
+                    Car = new Car
                     {
-                        ErrorMessage = ex.Message,
-                        StackTrace = ex.StackTrace
+                        CarId = Guid.NewGuid().ToString().ToLower(),
+                        Engine = new Car.Types.Engine
+                        {
+                            Cylinders = request.AddCar.Engine.Cylinders,
+                            Displacement = request.AddCar.Engine.Displacement,
+                            Horsepower = request.AddCar.Engine.Horsepower
+                        },
+                        Make = request.AddCar.Make.ToLower(),
+                        Name = request.AddCar.Name.ToLower(),
+                        Year = request.AddCar.Year
                     }
                 };
+
+                await _context.Cars.AddAsync(newCar);
+
+                var response = new CarResponse();
+                response.Car = newCar.Car;
+
+                return response;
             }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, $"Detected error while processing: {request}");
+                return new CarResponse { Error = GenerateErrorResponse(ex) };
+            }
+        }
+
+        private Error GenerateErrorResponse(Exception exception)
+        {
+            return new Error
+            {
+                ErrorCode = ErrorCode.InternalError,
+                ErrorMessage = exception.Message,
+                StackTrace = exception.StackTrace
+            };
         }
     }
 }
